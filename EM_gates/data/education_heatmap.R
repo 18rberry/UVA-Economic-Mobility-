b39a58e3b111eec6f898335fd9bfac8ff6787f5c
Martha Czernuszenko <mvczer@gmail.com>
2020-07-27 13:11:36 -0400

    Reading in questions automatically

--- EM_gates/data/education_heatmap.R
+++ EM_gates/data/education_heatmap.R
@@ -6,6 +6,10 @@ education <-read.csv("data/Education.csv")
 
 head(education)
 
+#get length of education
+nrow(education)
+education$X.POLICY.QUESTIONS.
+
 #AUTOMATE - unique values in dimension columns
 #dataframe$columnname
 #Lara/Tasfia
@@ -14,23 +18,30 @@ Dimensions<-rep(c("Voter Registration", "Voting Accessibility"), c(3*5, 3*4)) #3
 #First create a numeric vector the length of the number of questions and then assign the questions to the numeric vector,
 #so they will appear in the correct order. You need to use the factor to assign the questions to the numbers.
 
+
+#OLD CODE
 #AUTOMATE - unique function for question category
 #factor(c(1:nrow(df))),labels = c(unique function or iterating over a loop?) or count function
 #Martha
-Questions<-factor(c(1:9), labels=c("Does the state have automatic voter\nregistration at the DMV?",
-                                   "Does the state have automatic voter registration\nat agencies other than the DMV?",
-                                   "Does the state have online registration?",
-                                   "Does the state have Election Day\nvoter registration?",
-                                   "Is there pre-registration for individuals\n<18 years of age?",
-                                   "Does the state allow voting with no\nvoter ID documents?",
-                                   "Does the state have automatic no\nexcuse absentee/mail-in voting?",
-                                   "Does the state have 28 days or\nmore of early voting?",
-                                   "Does the state restore a felon's right to\nvote without a Governor's pardon?"))
+#Questions<-factor(c(1:9), labels=c("Does the state have automatic voter\nregistration at the DMV?",
+                                   #"Does the state have automatic voter registration\nat agencies other than the DMV?",
+                                   #"Does the state have online registration?",
+                                   #"Does the state have Election Day\nvoter registration?",
+                                   #"Is there pre-registration for individuals\n<18 years of age?",
+                                   #"Does the state allow voting with no\nvoter ID documents?",
+                                   #"Does the state have automatic no\nexcuse absentee/mail-in voting?",
+                                   #"Does the state have 28 days or\nmore of early voting?",
+                                   #"Does the state restore a felon's right to\nvote without a Governor's pardon?"))
 
+#NEW CODE TO REPLACE OLD CODE AND GET QUESTIONS
+Questions <- education$X.POLICY.QUESTIONS.
+Questions
+
+#ALSO EDITED THIS FOR NROW INSTEAD OF 9
 #Questions<-rep(Questions, rep(3, nrow(df))
-Questions<-rep(Questions, rep(3, 9)) #3=number of states, 9=number of questions
+Questions<-rep(Questions, rep(3, nrow(education))) #3=number of states, 9=number of questions
 #keep abbrevation
-States<-rep(c("VA","IA","OR"), 9) #9=number of questions
+States<-rep(c("VA","IA","OR"), nrow(education)) #9=number of questions
 
 #
 States<-ordered(States, levels=c("VA","IA","OR"))
