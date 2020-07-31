8fded55b520095514eee8c10a1b47130b2a0b8be
Riya Berry <rb4ba@virginia.edu>
2020-07-31 15:04:48 -0400

    import statements

--- EM_gates/plots/employment_lollipop.R
+++ EM_gates/plots/employment_lollipop.R
@@ -4,38 +4,38 @@ library(dplyr)
 library(ggplot2)
 
 
-#import data 
-data<- read_csv("~/git/dspg20uvaEM/EM_gates/data/Composite Scorecard - Sheet2.csv")
+#import data
+data<- read_csv("~/git/TestDSPG/dspg20uvaEM/EM_gates/data/Composite Scorecard - Sheet2.csv")
 
 #slice the data to just your domain
 # for example, for law encforcement i'm slicing from 1:20
 
 emp_data <- data %>%
   slice(9:11)
-
+emp_data
 #from your domain-specific data, slice for just the composite row (don't filter because that may be wierd)
 composite_emp <- data %>%
   slice(12)
-
+composite_emp
 #gather the data to compress the diff scores into one state and score variable
 plot_data_1 <- emp_data %>%
   gather("state", "score", c(3:5))
 
 plot_data_1 %>%
   ggplot(aes(x=score, y= state)) +
-  geom_point(aes(colour = state)) +  
-  geom_segment( aes(x= 0, xend= score, y= state, yend= state, colour = state)) 
+  geom_point(aes(colour = state)) +
+  geom_segment( aes(x= 0, xend= score, y= state, yend= state, colour = state))
 
-#to get a subdomain-wise plot slice the data for your domain for just the row that pertains to the specific subdomain 
+#to get a subdomain-wise plot slice the data for your domain for just the row that pertains to the specific subdomain
 org <- emp_data %>%
   slice(1)
 
-#and then follow the same process as before  
+#and then follow the same process as before
 plot_data_2 <- org %>%
   gather("state", "score", c(3:5))
 
 org_plot <- plot_data_2 %>%
   ggplot(aes(x=score, y= state)) +
-  geom_point(aes(colour = state)) +  
-  geom_segment( aes(x= 0, xend= score, y= state, yend= state, colour = state)) 
+  geom_point(aes(colour = state)) +
+  geom_segment( aes(x= 0, xend= score, y= state, yend= state, colour = state))
 
