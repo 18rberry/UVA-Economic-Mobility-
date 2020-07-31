abb1afe8e4a1a96a7df88ff2d7011abdbf0b7c07
Lara <llhaase@gmail.com>
2020-07-31 00:32:54 -0400

    Create employment heatmap

--- EM_gates/plots/employment_lollipop.R
+++ EM_gates/plots/employment_lollipop.R
@@ -1,37 +1,40 @@
 library(tidyr)
 library(readr)
+library(dplyr)
+library(ggplot2)
+
+
 #import data 
+data<- read_csv("~/git/dspg20uvaEM/EM_gates/data/Composite Scorecard - Sheet2.csv")
 
 #slice the data to just your domain
 # for example, for law encforcement i'm slicing from 1:20
 
-law_data <- em_data %>%
-  slice(10:13)
+emp_data <- data %>%
+  slice(9:11)
 
 #from your domain-specific data, slice for just the composite row (don't filter because that may be wierd)
-composite_law <- law_data %>%
-  slice(4)
+composite_emp <- data %>%
+  slice(12)
 
 #gather the data to compress the diff scores into one state and score variable
-plot_data_1 <- composite_law %>%
+plot_data_1 <- emp_data %>%
   gather("state", "score", c(3:5))
 
-
-law_domain_plot <- plot_data_1 %>%
+plot_data_1 %>%
   ggplot(aes(x=score, y= state)) +
   geom_point(aes(colour = state)) +  
   geom_segment( aes(x= 0, xend= score, y= state, yend= state, colour = state)) 
 
 #to get a subdomain-wise plot slice the data for your domain for just the row that pertains to the specific subdomain 
-arrest <- law_data %>%
+org <- emp_data %>%
   slice(1)
 
-
 #and then follow the same process as before  
-plot_data_2 <- arrest %>%
+plot_data_2 <- org %>%
   gather("state", "score", c(3:5))
 
-arrest_plot <- plot_data_2 %>%
+org_plot <- plot_data_2 %>%
   ggplot(aes(x=score, y= state)) +
   geom_point(aes(colour = state)) +  
   geom_segment( aes(x= 0, xend= score, y= state, yend= state, colour = state)) 
